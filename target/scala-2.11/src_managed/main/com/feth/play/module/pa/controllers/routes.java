7efdd1c40621dbc685539f9ed0a61a64f4f01dc0
quanhd <quanhd0103@gmail.com>
2016-10-02 22:54:51 -0700

    1

---
+++ target/scala-2.11/src_managed/main/com/feth/play/module/pa/controllers/routes.java
@@ -0,0 +1,20 @@
+// @SOURCE:/Users/quanhdo/Projects/play-authenticate/conf/routes
+// @HASH:6caaefd4ef4f1d428984c768556744679d050c00
+// @DATE:Sun Oct 02 22:50:00 PDT 2016
+
+package com.feth.play.module.pa.controllers;
+
+public class routes {
+public static final com.feth.play.module.pa.controllers.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.ReverseAuthenticate();
+
+public static class javascript {
+public static final com.feth.play.module.pa.controllers.javascript.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.javascript.ReverseAuthenticate();
+}
+          
+
+public static class ref {
+public static final com.feth.play.module.pa.controllers.ref.ReverseAuthenticate Authenticate = new com.feth.play.module.pa.controllers.ref.ReverseAuthenticate();
+}
+          
+}
+          
